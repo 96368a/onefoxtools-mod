a5b4ecee57086ac9b6ce544a222502232a02b0d1
96368a <ncxxg@qq.com>
2023-09-28 15:05:43 +0800

    :lipstick: 加入消息弹窗组件

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import { Router, useRoutes } from '@solidjs/router'
 import { onMount } from 'solid-js'
 import { themeChange } from 'theme-change'
+import toast, { Toaster } from 'solid-toast'
 import Footer from './components/Footer'
 import routes from '~solid-pages'
 
@@ -8,9 +9,11 @@ export default function App() {
   const Routes = useRoutes(routes)
   onMount(async () => {
     themeChange()
+    toast.success('加载完成')
   })
   return (
     <main class="font-sans text-center text-gray-700 dark:text-gray-200 bg-base-200">
+      <Toaster/>
       <Router>
         <Routes />
       </Router>
